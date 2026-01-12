59295db868ae0f64f517f26d5d2da2f470df2b85
Sebastian <Leviathan1980@gmail.com>
2026-01-12 18:13:10 -0300

    Introduce intentional failure to validate CI reporting

--- test/specs/sanity/login.sanity.spec.js
+++ test/specs/sanity/login.sanity.spec.js
@@ -1,42 +1,29 @@
 const LoginPage = require('../../pageobjects/pages/LoginPage')
 const { loginData } = require('../../data/login.data')
 
-describe('Sanity - Login', () => {
+describe('@sanity - Login', () => {
 
   describe('Valid credentials', () => {
-
     loginData.validUsers.forEach((user) => {
-
       it(`should login successfully: ${user.description}`, async () => {
         await LoginPage.open()
         await LoginPage.login(user.username, user.password)
 
-        await expect(LoginPage.flashMessage).toBeDisplayed()
-        await expect(LoginPage.flashMessage).toHaveText(user.expectedMessage, { containing: true })
-
-
+        await LoginPage.expectLoginSucceeded(user.expectedMessage)
+        await expect(page.flashMessage).toHaveTextContaining('THIS SHOULD FAIL')
       })
-
     })
-
   })
 
   describe('Invalid credentials', () => {
-
     loginData.invalidUsers.forEach((user) => {
-
       it(`should NOT login: ${user.description}`, async () => {
         await LoginPage.open()
         await LoginPage.login(user.username, user.password)
 
-        await expect(LoginPage.flashMessage).toBeDisplayed()
-        await expect(LoginPage.flashMessage).toHaveText(user.expectedMessage, { containing: true })
-
-
+        await LoginPage.expectLoginFailed(user.expectedMessage)
       })
-
     })
-
   })
 
 })
