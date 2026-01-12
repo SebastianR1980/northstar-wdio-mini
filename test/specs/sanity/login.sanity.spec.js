b337e48e1cc8eead677a8a29a839ffefb391d299
Sebastian <Leviathan1980@gmail.com>
2026-01-12 18:17:59 -0300

    Revert "Introduce intentional failure to validate CI reporting"
    This reverts commit 59295db868ae0f64f517f26d5d2da2f470df2b85.

--- test/specs/sanity/login.sanity.spec.js
+++ test/specs/sanity/login.sanity.spec.js
@@ -1,29 +1,42 @@
 const LoginPage = require('../../pageobjects/pages/LoginPage')
 const { loginData } = require('../../data/login.data')
 
-describe('@sanity - Login', () => {
+describe('Sanity - Login', () => {
 
   describe('Valid credentials', () => {
+
     loginData.validUsers.forEach((user) => {
+
       it(`should login successfully: ${user.description}`, async () => {
         await LoginPage.open()
         await LoginPage.login(user.username, user.password)
 
-        await LoginPage.expectLoginSucceeded(user.expectedMessage)
-        await expect(page.flashMessage).toHaveTextContaining('THIS SHOULD FAIL')
+        await expect(LoginPage.flashMessage).toBeDisplayed()
+        await expect(LoginPage.flashMessage).toHaveText(user.expectedMessage, { containing: true })
+
+
       })
+
     })
+
   })
 
   describe('Invalid credentials', () => {
+
     loginData.invalidUsers.forEach((user) => {
+
       it(`should NOT login: ${user.description}`, async () => {
         await LoginPage.open()
         await LoginPage.login(user.username, user.password)
 
-        await LoginPage.expectLoginFailed(user.expectedMessage)
+        await expect(LoginPage.flashMessage).toBeDisplayed()
+        await expect(LoginPage.flashMessage).toHaveText(user.expectedMessage, { containing: true })
+
+
       })
+
     })
+
   })
 
 })
